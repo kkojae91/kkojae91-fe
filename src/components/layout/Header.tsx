92e2d7546f93d1ac9ae8918c2b3f2cc84538ac72
kkojae <jaejeung3210@gmail.com>
2023-01-28 22:08:58 +0900

    feat: login 시 Header에 userName 노출 및 logout 기능 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,45 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import { PATHS } from '../../constants/path';
+
+import useSetAuthorization from '../../hooks/useSetAuthorization';
 
 const Header = () => {
+  const { isLogin, userName, logout } = useSetAuthorization();
+
   return (
     <Container>
-      <Link href='/'>
-        <Title>HAUS</Title>
-      </Link>
+      <Title>
+        <Link href={PATHS.HOME}>HAUS</Link>
+      </Title>
 
-      <Link href='/login'>
-        <p>login</p>
-      </Link>
+      {isLogin && (
+        <div>
+          <p>{userName}</p>
+          <button type='button' onClick={logout}>
+            logout
+          </button>
+        </div>
+      )}
+
+      {!isLogin && (
+        <p>
+          <Link href={PATHS.LOGIN}>login</Link>
+        </p>
+      )}
     </Container>
   );
 };
 
 export default Header;
 
-const Container = styled.div`
+const Container = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 20px;
 `;
 
-const Title = styled.a`
+const Title = styled.h1`
   font-size: 48px;
 `;

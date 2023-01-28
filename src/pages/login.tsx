f44bdd1f5bee3562af713cd806dd16e934f268b8
kkojae <jaejeung3210@gmail.com>
2023-01-28 16:14:20 +0900

    refactor: loginPage 마크업 수정 및 useControlledForm custom hook, Input component 적용

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,29 +2,62 @@ import type { NextPage } from 'next';
 import React from 'react';
 import styled from 'styled-components';
 
+import Input from '../components/Input';
+import useControlledForm from '../hooks/useControlledForm';
+import { ValuesType } from '../types/login';
+import { loginValidator } from '../utilities/validate';
+
 const LoginPage: NextPage = () => {
+  const { errorMessages, handleSubmit, touched, getProps } = useControlledForm<ValuesType>({
+    initialValues: { id: '', password: '' },
+    validate: loginValidator,
+    onSubmit: (values: ValuesType) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <Form>
-      <div>아이디</div>
-      <TextInput type='text' />
-      <div>비밀번호</div>
-      <TextInput type='password' />
-      <LoginButton disabled>로그인</LoginButton>
-    </Form>
+    <Container onSubmit={handleSubmit}>
+      <InputContainer>
+        <Input
+          id='id'
+          type='text'
+          labelText='아이디'
+          isInvalid={touched.id && errorMessages.id.length !== 0}
+          errorMessage={errorMessages.id}
+          getProps={getProps}
+        />
+
+        <Input
+          id='password'
+          type='password'
+          labelText='비밀번호'
+          isInvalid={touched.password && errorMessages.password.length !== 0}
+          errorMessage={errorMessages.password}
+          getProps={getProps}
+        />
+      </InputContainer>
+
+      <LoginButton type='submit' disabled={Object.values(errorMessages).some((value) => value)}>
+        로그인
+      </LoginButton>
+    </Container>
   );
 };
 
 export default LoginPage;
 
-const Form = styled.div`
+const Container = styled.form`
   display: flex;
   flex-direction: column;
   margin-top: 40px;
   padding: 0 20px 40px;
 `;
 
-const TextInput = styled.input`
-  border: 1px solid #000;
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 `;
 
 const LoginButton = styled.button`

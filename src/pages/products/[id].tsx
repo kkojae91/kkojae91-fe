119e008a5f6c2f43c8169851d8d0c8d0679fb7ff
kkojae <jaejeung3210@gmail.com>
2023-01-29 15:01:17 +0900

    feat: ProductDetail page component 개발 및 에러, 로딩 처리

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,44 +1,51 @@
-import Link from 'next/link';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
 
-import products from '../../api/data/products.json';
+import ExceptionError from '../../components/ExceptionError';
+import ProductDetailSkeleton from '../../components/ProductDetailSkeleton';
+import { ERROR_MESSAGES, REDIRECT_TEXT } from '../../constants/message';
+import { PATHS } from '../../constants/path';
+import useGetProductDetail from '../../hooks/queries/useGetProductDetail';
 
 const ProductDetailPage: NextPage = () => {
-  const product = products[0];
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, isLoading, isError } = useGetProductDetail({ id: Number(id ?? 1) });
+
+  const product = data?.data.product;
+
+  if (isError) {
+    return (
+      <ExceptionError
+        title={ERROR_MESSAGES.NOT_FOUND_PAGE}
+        description={REDIRECT_TEXT.TO_HOME}
+        buttonText='홈으로 가기'
+        path={PATHS.HOME}
+      />
+    );
+  }
+
+  if (isLoading) {
+    return <ProductDetailSkeleton />;
+  }
 
   return (
-    <>
-      <Header>
-        <Link href='/'>
-          <Title>HAUS</Title>
-        </Link>
-        <Link href='/login'>
-          <p>login</p>
-        </Link>
-      </Header>
-      <Thumbnail src={product.thumbnail ? product.thumbnail : '/defaultThumbnail.jpg'} />
+    <Container>
+      <Thumbnail src={product?.thumbnail ? product.thumbnail : '/defaultThumbnail.jpg'} />
+
       <ProductInfoWrapper>
-        <Name>{product.name}</Name>
-        <Price>{product.price}원</Price>
+        <Name>{product?.name}</Name>
+        <Price>{product?.price.toLocaleString('ko-KR')}원</Price>
       </ProductInfoWrapper>
-    </>
+    </Container>
   );
 };
 
 export default ProductDetailPage;
 
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-`;
-
-const Title = styled.a`
-  font-size: 48px;
-`;
+const Container = styled.article``;
 
 const Thumbnail = styled.img`
   width: 100%;
@@ -50,12 +57,12 @@ const ProductInfoWrapper = styled.div`
   padding: 0 20px;
 `;
 
-const Name = styled.div`
+const Name = styled.h1`
   font-size: 20px;
   font-weight: bold;
 `;
 
-const Price = styled.div`
+const Price = styled.p`
   font-size: 18px;
   margin-top: 8px;
 `;

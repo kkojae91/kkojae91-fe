ced66582251c2ee92f02151923a9b6979d8b9dbc
kkojae <jaejeung3210@gmail.com>
2023-01-30 13:21:43 +0900

    refactor: pagination 5배수에서 이전 버튼 클릭시 발생하는 버그 수정
    - 5배수에서 이전 버튼 클릭시 이전 pagination 보여지지 않는 문제 해결

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -12,6 +12,7 @@ export const parseQueryString = (search: string): Record<string, string> =>
       return acc;
     }, {} as Record<string, string>);
 
-export const floorBy5Unit = (num: number) => Math.floor(num / 5) * 5;
+export const floorBy5Unit = (num: number) =>
+  num % 5 === 0 ? Math.floor((num - 1) / 5) * 5 : Math.floor(num / 5) * 5;
 
 export const ceilBy5Unit = (num: number) => Math.ceil(num / 5) * 5;
